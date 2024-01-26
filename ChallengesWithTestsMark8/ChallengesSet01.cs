43b905db985c377186847274633221963a1cd64f
will street <will@williamdstreet.com>
2024-01-26 13:37:46 -0600

    first week

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -4,6 +4,7 @@ namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
     {
+        //PASSED///////////////////////////////////////////////////////////////////////////
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
             if(num1 == num2)
@@ -16,34 +17,51 @@ namespace ChallengesWithTestsMark8
             }
         }
 
+        //PASSED///////////////////////////////////////////////////////////////////////
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
+        //PASSED//////////////////////////////////////////////////////////////////////
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
+        //PASSED//////////////////////////////////////////////////////////////////////
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else { return number1; }
         }
 
+        //PASSED//////////////////////////////////////////////////////////////////////
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
+        //PASSED//////////////////////////////////////////////////////////////////////
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson != "")
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+            else
+            {
+                return "Hello!";
+            }
         }
 
+        //PASSED/////////////////////////////////////////////////////////////////////////
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
